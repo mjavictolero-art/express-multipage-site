aeac82d3736af9e8c5806b744abea60a6bafa0c9
Justine Ace Victolero <justineacevictolero24@gmail.com>
2026-02-13 20:40:10 +0800

    First commit

--- api/about.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'views', 'about.html');
-  const content = fs.readFileSync(filePath, 'utf8');
-  res.status(200).send(content);
-}
