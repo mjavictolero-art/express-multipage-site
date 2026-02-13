5d841f049bd8346c0a65bc5b360db953d62ed838
Justine Ace Victolero <justineacevictolero24@gmail.com>
2026-02-13 20:32:43 +0800

    Convert to Vercel serverless API

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'views', 'index.html');
+  const content = fs.readFileSync(filePath, 'utf8');
+  res.status(200).send(content);
+}
